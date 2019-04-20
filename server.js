540eaaba5ebfe94ab62ecfcff3664f799dbb3215
Laura Lively <Laura.Lively@gmail.com>
2019-04-20 16:26:54 -0600

    Changed Server.js

--- server.js
+++ server.js
@@ -1,17 +1,30 @@
-'use strict';
 
-const express = require('express');
-
-// Constants
-const PORT = 3000;
-const HOST = '0.0.0.0';
-
-// App
-const app = express();
-app.get('/', (req, res) => {
-  res.send('Hello Node.js Sample!\n');
-});
-
-var port = process.env.PORT||PORT;
-app.listen(port);
-console.log(`Running on http://${HOST}:${PORT}`);
+// ===[ DEPENDENCIES ]===============================
+const express = require('express')
+const bodyParser = require('body-parser')
+const path = require('path')
+const app = express()
+ 
+ 
+ 
+// ===[ MIDDLEWARE ]=================================
+app.use(express.static(path.join(__dirname, 'build')))
+ 
+ 
+ 
+// ===[ ROUTES ]=====================================
+app.get('/ping', function (req, res) {  // Just to test server
+ return res.send('pong');
+})
+ 
+app.get('/', function (req, res) {      // Route to serve build React-App.
+  res.sendFile(path.join(__dirname, 'build', 'index.html'))
+})
+ 
+ 
+ 
+// === [ SERVER LISTENER ]===========================
+app.listen((process.env.PORT || 3000), (err) => {
+    if(err){ throw err }
+    console.log("Server Listening on port: 3000 ....")
+})
