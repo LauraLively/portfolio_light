d0ce7ec3827ff26ed58d8bb824af007be936b025
Laura Lively <Laura.Lively@gmail.com>
2019-04-20 16:21:08 -0600

    added prod build

---
+++ build/precache-manifest.c88c26b4faacf8903b3912baaca15834.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "aa8e2ce474cd324e70446cf20b0e1731",
+    "url": "/static/media/DSC_6680.aa8e2ce4.jpg"
+  },
+  {
+    "revision": "6c2abd5f1cc009b86c51",
+    "url": "/static/css/main.ffba022c.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f9f1a4b8ea26f43044cd",
+    "url": "/static/js/2.ca2082a2.chunk.js"
+  },
+  {
+    "revision": "a97fe21f8529f4590caa21470cc15809",
+    "url": "/static/media/Avatar.a97fe21f.png"
+  },
+  {
+    "revision": "2b2ea7902617111bbd479b55fda9d1b4",
+    "url": "/static/media/Desktop_Turquois_1280.2b2ea790.png"
+  },
+  {
+    "revision": "226fa3453b0550f0b2c2e314f4cc5be5",
+    "url": "/static/media/Fall_Bouquet_trello.226fa345.png"
+  },
+  {
+    "revision": "6f370fe1018ff853bed79aee7dbc9e29",
+    "url": "/static/media/iphone_stars.6f370fe1.png"
+  },
+  {
+    "revision": "6c2abd5f1cc009b86c51",
+    "url": "/static/js/main.e3b5ad05.chunk.js"
+  },
+  {
+    "revision": "ced892d97cd9a707555f5851d144e441",
+    "url": "/static/media/computer-2930704_1280.ced892d9.jpg"
+  },
+  {
+    "revision": "b332eba78efe0dabea4c8439fba27681",
+    "url": "/static/media/DSC_6680.b332eba7.png"
+  },
+  {
+    "revision": "12ddbeeb28ffdf5efb58d910526bbca8",
+    "url": "/static/media/monitor-862116_1920.12ddbeeb.jpg"
+  },
+  {
+    "revision": "6f23a622bc4a404bf8f85266471359a3",
+    "url": "/static/media/sky-828648_1920.6f23a622.jpg"
+  },
+  {
+    "revision": "f7c2603a131ec73adac488a0c95776df",
+    "url": "/static/media/computer-2930704_1280.f7c2603a.png"
+  },
+  {
+    "revision": "780e5df0ec2789c39d0e5fcffa7a9a3d",
+    "url": "/static/media/monitor-862116_1920.780e5df0.png"
+  },
+  {
+    "revision": "d205b45fa827b6e052542a8d9ecd1f6a",
+    "url": "/index.html"
+  }
+];
--- server.js
+++ server.js
@@ -1,30 +1,17 @@
+'use strict';
 
-// ===[ DEPENDENCIES ]===============================
-const express = require('express')
-const bodyParser = require('body-parser')
-const path = require('path')
-const app = express()
- 
- 
- 
-// ===[ MIDDLEWARE ]=================================
-app.use(express.static(path.join(__dirname, 'build')))
- 
- 
- 
-// ===[ ROUTES ]=====================================
-app.get('/ping', function (req, res) {  // Just to test server
- return res.send('pong');
-})
- 
-app.get('/', function (req, res) {      // Route to serve build React-App.
-  res.sendFile(path.join(__dirname, 'build', 'index.html'))
-})
- 
- 
- 
-// === [ SERVER LISTENER ]===========================
-app.listen((process.env.PORT || 8080), (err) => {
-    if(err){ throw err }
-    console.log("Server Listening on port: 8080 ....")
-})
+const express = require('express');
+
+// Constants
+const PORT = 3000;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello Node.js Sample!\n');
+});
+
+var port = process.env.PORT||PORT;
+app.listen(port);
+console.log(`Running on http://${HOST}:${PORT}`);
